93cd72d96bbb8698b575881a44aef3bb0a39f674
우성원 <frosttvinkorea@gmail.com>
2023-01-15 23:42:01 +0900

    리듬게임 만들기 2
    노트 클래스를 만들고, 떨어트리는 로직을 만듭니다. 아직 갈 길이 멉니다.

--- import pygame.py
+++ import pygame.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import math
+import random
 import time
 import os
 
@@ -21,77 +22,67 @@ FRAME_HEIGHT = 900
 LINE_WIDTH = 5
 KEY_SPACE = 10
 
-class Key: # 판정선 아래 네모들
+class Note(pygame.sprite.Sprite): # 노트 클래스
+    def __init__(self, lane, speed):
+        super().__init__()
+        note_images_list = [resource_path("note1.png"), resource_path("note2.png")]
+        image_selected = random.choice(note_images_list)
+        self.image = pygame.image.load(image_selected)
+        self.rect = self.image.get_rect()
+        self.rect.x = FRAME_X + FRAME_WIDTH/4*lane
+        self.rect.y = -20
+        self.speed = speed
+        
+    def draw(self, screen):
+        pygame.draw.rect(screen, WHITE, self.image)
+    
+ #   def note_decision(self, line): # 노트 판정
+  #      if self.rect 
+        
+    def update(self): # 노트 떨어트리기(스피드)
+        self.rect.y += self.speed
+
+class Game():
     def __init__(self):
-        font_path = resource_path("DELTABEAT/big_noodle_titling.ttf")
+        font_path = resource_path("big_noodle_titling.ttf")
+        press_se = resource_path("snare.wav")
+        self.press = pygame.mixer.Sound(press_se)
         self.font = pygame.font.Font(font_path, 50)
+        self.notes = pygame.sprite.Group()
+        
+    def game_logic(self):
+        self.notes.add(Note(2, 1))
+        self.notes.update()
+            
+        
+    def display_object(self, screen):
+        self.notes.draw(screen)
+        
+    def display_frame(self, screen, keycolor, fontcolor): #게임 프레임 그리기 - 플레이하는 부분
+        screen.fill(BLACK)
         x = FRAME_X + KEY_SPACE
         y = FRAME_HEIGHT * 7/9 + 40
         keyframe_size = FRAME_WIDTH/4 - KEY_SPACE*2
-        self.d = pygame.Rect(x, y, keyframe_size, keyframe_size)
-        self.f = pygame.Rect(x+(FRAME_WIDTH/4), y, keyframe_size, keyframe_size)
-        self.j = pygame.Rect(x+(FRAME_WIDTH*2/4), y, keyframe_size, keyframe_size)
-        self.k = pygame.Rect(x+(FRAME_WIDTH*3/4), y, keyframe_size, keyframe_size)
-        
-    
-    def draw(self, screen, color, fontcolor):
-        dkey = self.font.render("D", True, fontcolor)
-        fkey = self.font.render("F", True, fontcolor)
-        jkey = self.font.render("J", True, fontcolor)
-        kkey = self.font.render("K", True, fontcolor)
-        
-        width = dkey.get_width()
-        height = dkey.get_height()
-        x = self.d.centerx
-        y = self.d.centery
-        xpos = x - int(width / 2)
-        ypos = y - int(height / 2)
-        pygame.draw.rect(screen, color, self.d)
-        pygame.draw.rect(screen, color, self.f)
-        pygame.draw.rect(screen, color, self.j)
-        pygame.draw.rect(screen, color, self.k)
-        screen.blit(dkey, (xpos, ypos))
-        screen.blit(fkey, (xpos + (FRAME_WIDTH/4), ypos))
-        screen.blit(jkey, (xpos + (FRAME_WIDTH*2/4), ypos))
-        screen.blit(kkey, (xpos + (FRAME_WIDTH*3/4), ypos))
-        
-
-class Game:
-    def __init__(self):
-        font_path = resource_path("DELTABEAT/big_noodle_titling.ttf")
-        press_se = resource_path("DELTABEAT/snare.wav")
-        self.press = pygame.mixer.Sound(press_se)
-        self.font = pygame.font.Font(font_path, 50)
-        self.key = Key()
-
-    def draw_map(self, screen): #게임 프레임 그리기 - 플레이하는 부분
-        for i in range(3):
+        for i in range(3): #노트 구분 선
             pygame.draw.line(screen, GRAY, [FRAME_X + (i + 1)*(FRAME_WIDTH/4), 0], [FRAME_X + (i + 1)*(FRAME_WIDTH/4), FRAME_HEIGHT + LINE_WIDTH*2], width=1)
-        pygame.draw.rect(screen, OCEAN_BLUE, [FRAME_X, FRAME_HEIGHT* 7/9, FRAME_WIDTH, 20])
+            
+        pygame.draw.rect(screen, OCEAN_BLUE, [FRAME_X, FRAME_HEIGHT* 7/9, FRAME_WIDTH, 20])  #판정선 그리기
         for i in range(20):
             pygame.draw.line(screen, SKY_BLUE, [FRAME_X + i*20 + 20, FRAME_HEIGHT* 7/9], [FRAME_X + i*20 +10, FRAME_HEIGHT* 7/9 + 20], width = 10)
-        self.key.draw(screen , LEAF_GREEN , WHITE)
+        
+        #게임 프레임
         pygame.draw.rect(screen, WHITE, [FRAME_X - LINE_WIDTH, -1 * LINE_WIDTH, FRAME_WIDTH + LINE_WIDTH*2, FRAME_HEIGHT + LINE_WIDTH*2], width=LINE_WIDTH)
         pygame.draw.rect(screen, WHITE, [(FRAME_X + FRAME_WIDTH), (FRAME_HEIGHT / 2), 20, (FRAME_HEIGHT / 2) + LINE_WIDTH], width = LINE_WIDTH)
-
+        for i, key in enumerate(["D", "F", "J", "K"]): #게임 판정선 아래 키 설명
+            pygame.draw.rect(screen, keycolor, [x+FRAME_WIDTH*i/4, y, keyframe_size, keyframe_size])
+            keyset = self.font.render(key, True, fontcolor)
+            screen.blit(keyset, (x+FRAME_WIDTH*i/4+keyframe_size/2-KEY_SPACE, y+KEY_SPACE))
+            
     def process_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return True
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_d:
-                    self.press.play()
-                if event.key == pygame.K_f:
-                    self.press.play()
-                if event.key == pygame.K_j:
-                    self.press.play()
-                if event.key == pygame.K_k:
-                    self.press.play()
-
-
-
-
-
+        
 
 def resource_path(relative_path):
     try:
@@ -100,44 +91,20 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
     return os.path.join(base_path, relative_path)
 
-
-
-
-
-
 def main():
     pygame.init()
     pygame.display.set_caption("DELTABEAT")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    font = pygame.font.Font(None, 40)
-
-    try:
-        pygame.mixer.music.load("DELTABEAT\Children_Record.wav")
-    except:
-        print("ogg 파일이 맞지 않거나, 오디오 기기와 접속되어 있지 않습니다.")
-    
     game = Game()
     done = False
 
     while not done:
         done = game.process_events()
-        key = pygame.key.get_pressed()
-        if key[pygame.K_p] == 1:
-            if pygame.mixer.music.get_busy() == False:
-                pygame.mixer.music.play(-1)
-        if key[pygame.K_s] == 1:
-            if pygame.mixer.music.get_busy() == True:
-                pygame.mixer.music.stop()
-
-        pos = pygame.mixer.music.get_pos()
-        txt1 = font.render("BGM pos" + str(pos / 1000), True, WHITE)
-        txt2 = font.render("[P]lay bgm : [S]top bgm", True, CYAN)
-        screen.fill(BLACK)
-        game.draw_map(screen)
-        screen.blit(txt1, [100, 100])
-        screen.blit(txt2, [100, 200])
-        pygame.display.update()
+        game.display_frame(screen, LEAF_GREEN, WHITE)
+        game.game_logic()
+        game.display_object(screen)
+        pygame.display.flip()
         clock.tick(60)
 
     pygame.quit()
